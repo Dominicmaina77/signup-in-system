89723b880572f3545b724d6d84ad92fd6c03fe02
Dominicmaina77 <mainadominic628@gmail.com>
2025-11-11 15:57:13 +0300

    connecttodatabase

--- db.sql
+++ db.sql
@@ -2,12 +2,17 @@ CREATE DATABASE jwttutorial;
 
 -- set extension
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE users(
-    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_name VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL ,
-    user_password VARCHAR(255) NOT NULL
-);
+
+CREATE TABLE
+    users (
+        user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+        user_name VARCHAR(255) NOT NULL,
+        user_email VARCHAR(255) NOT NULL,
+        user_password VARCHAR(255) NOT NULL
+    );
 
 -- insert exmple users
-INSERT INTO users(user_name,user_email,user_password) VALUES('james','james@gmail.com', '254james');
+INSERT INTO
+    users (user_name, user_email, user_password)
+VALUES
+    ('james', 'james@gmail.com', '254james');
