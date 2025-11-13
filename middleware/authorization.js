b0991e97e146cec4b64aeab9da68c55ec7c25950
Dominicmaina77 <mainadominic628@gmail.com>
2025-11-13 11:04:47 +0300

    authorization

---
+++ middleware/authorization.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports= async(req,res,next)=>{
+    // try {
+    //     const token = req.headers.authorization.split(" ")[1];
+    //     const isCustomAuth = token.length < 500;
+    //     let decodedData;
+    //     if(token && isCustomAuth){
+    //         decodedData = jwt.verify(token,'secret');
+    //         req.userId = decodedData?.id;
+    //     }
+    //     next();
+    // } catch (error) {
+    //     // console.log(error);
+    //     res.status(403).send({message:"Unauthorized access"});
+    // }
+
+    try {
+        const jwtToken = req.headers("token");
+
+        if(!jwtToken){
+            return res.status(403).send({message:"Unauthorized access"});
+        }
+        const payload = jwtToken.verify(jwtToken,process.env.jwtSecret);
+        req.user=payload.user;
+    } catch (err) {
+        console.error(err.message);
+        return res.status(403).json("Unauthorized access");
+    }
+}
